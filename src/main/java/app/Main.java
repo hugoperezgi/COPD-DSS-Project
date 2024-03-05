d5c0a7f503c41192848b9005580b41a98318ec4f
hugop <h.perez10@usp.ceu.es>
2024-03-05 09:35:06 +0100

    JavaFX should launch now, keyword should.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -14,15 +14,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/LoginController.fxml"));
-		Parent root = loader.load();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/gui/LoginController.fxml"));
+        Parent root = loader.load();
 
-        Image windowIcon = new Image("img/logo.png");
+        Image windowIcon = new Image("/img/logo.png");
         primaryStage.getIcons().add(windowIcon);	
 
         primaryStage.setTitle("COPD Decision Support System"); 
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
     }
 }
