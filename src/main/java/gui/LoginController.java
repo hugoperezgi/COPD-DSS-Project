d7b24a1f97406ba4f85073151b8ebdbf8af42504
hugop <h.perez10@usp.ceu.es>
2024-03-16 22:36:04 +0100

    javafx - login

--- src/main/java/gui/LoginController.java
+++ src/main/java/gui/LoginController.java
@@ -21,78 +21,76 @@ public class LoginController {
 
     private ErrorPopup ErrorPopup = new ErrorPopup();
 
-    // public static BIT b;
-
 
     public void logIn(ActionEvent a) throws IOException, ClassNotFoundException{
-        // Integer control = ClientLogic.logIn(username.getText(),psw.getText());
-        // switch (control) {
-        //     case -2:
-        //         ErrorPopup.errorPopup(5);
-        //         break;
-        //     case 0:
-        //         // b.gtfo();
-        //         loadAdminUI(a);
-        //         break;
-        //     case 1:
-        //         // b.gtfo();
-        //         loadDoctUI(a);
-        //         break;
-        //     case 2:
-        //         loadPatientUI(a);
-        //         break;
         
-        //     default:
-        //         ErrorPopup.errorPopup(0);
-        //         break;
-        // }
+        Integer control = -1;
+        //control= TODO idk-idc.logIn(username.getText(),psw.getText()); 
+        switch (control) {
+            case -2:
+                ErrorPopup.errorPopup(5);
+                break;
+            case 0:
+                loadAdminUI(a);
+                break;
+            case 1:
+                loadDoctUI(a);
+                break;
+            case 2:
+                loadPatientUI(a);
+                break;
+        
+            default:
+                ErrorPopup.errorPopup(0);
+                break;
+        }
     }
     
 
-    // private void loadPatientUI(ActionEvent aEvent) throws IOException, ClassNotFoundException{
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("PatientController.fxml"));
-	// 	Parent root = loader.load();
+    private void loadPatientUI(ActionEvent aEvent) throws IOException, ClassNotFoundException{
+		// FXMLLoader loader = new FXMLLoader(getClass().getResource("PatientController.fxml"));
+		// Parent root = loader.load();
 
-    //     // PatientController pc = loader.getController();
-    //     pc.setSelf();
-    //     if(pc.myself==null){
-    //         ErrorPopup.errorPopup(11); //TODO
-    //     } else {
-    //         Stage stage = (Stage) ((Node) aEvent.getSource()).getScene().getWindow();
-    //         Scene scene = new Scene(root);
-    //         stage.setScene(scene);
-    //         stage.show();
-    //     }
+        // PatientController pc = loader.getController();
+        // pc.setSelf();
+        // if(pc.myself==null){
+        //     ErrorPopup.errorPopup(11); //TODO
+        // } else {
+        //     Stage stage = (Stage) ((Node) aEvent.getSource()).getScene().getWindow();
+        //     Scene scene = new Scene(root);
+        //     stage.setScene(scene);
+        //     stage.show();
+        // }
 
-    // }
+    }
 
-    // private void loadAdminUI(ActionEvent aEvent) throws IOException{
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminController.fxml"));
-	// 	Parent root = loader.load();
+    private void loadAdminUI(ActionEvent aEvent) throws IOException{
+		// FXMLLoader loader = new FXMLLoader(getClass().getResource("AdminController.fxml"));
+		// Parent root = loader.load();
 
-    //     Stage stage = (Stage) ((Node) aEvent.getSource()).getScene().getWindow();
-    //     Scene scene = new Scene(root);
-    //     stage.setScene(scene);
-    //     stage.show();
+        // Stage stage = (Stage) ((Node) aEvent.getSource()).getScene().getWindow();
+        // Scene scene = new Scene(root);
+        // stage.setScene(scene);
+        // stage.show();
 
-    // }
+    }
 
-    // private void loadDoctUI(ActionEvent aEvent) throws IOException, ClassNotFoundException{
-	// 	FXMLLoader loader = new FXMLLoader(getClass().getResource("WorkerController.fxml"));
-	// 	Parent root = loader.load();
+    private void loadDoctUI(ActionEvent aEvent) throws IOException, ClassNotFoundException{
+		// FXMLLoader loader = new FXMLLoader(getClass().getResource("WorkerController.fxml"));
+		// Parent root = loader.load();
 
-    //     // WorkerController wc = loader.getController();
-    //     wc.setSelf();
-    //     if(wc.myself==null){
-    //         ErrorPopup.errorPopup(11);
-    //     } else {
-    //         Stage stage = (Stage) ((Node) aEvent.getSource()).getScene().getWindow();
-    //         Scene scene = new Scene(root);
-    //         stage.setScene(scene);
-    //         stage.show();
-    //     }
+        // DoctorController dc = loader.getController();
+        // dc.setSelf();
+        // if(dc.myself==null){
+        //     ErrorPopup.errorPopup(11);
+        // } else {
+        //     Stage stage = (Stage) ((Node) aEvent.getSource()).getScene().getWindow();
+        //     Scene scene = new Scene(root);
+        //     stage.setScene(scene);
+        //     stage.show();
+        // }
 
-    // }
+    }
 
 
 }
