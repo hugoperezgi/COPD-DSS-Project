cabb7a26a3cd375bd1e621e95d939ad9a3c7ef72
albaavl <a.vargas10@usp.ceu.es>
2024-03-17 22:14:51 +0100

    mas cosas de javafx pero sigo sin saber hacer otras @hugo

--- src/main/java/gui/PatientController.java
+++ src/main/java/gui/PatientController.java
@@ -1,7 +1,6 @@
 package gui;
 
 import java.net.URL;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -21,29 +20,33 @@ public class PatientController implements Initializable{
     @FXML
     public Pane paneShowMedicalHist;
     @FXML
-    public ComboBox<Integer> medicalHistoryIDs;
+    public ComboBox<Integer> comboBoxMedicalHistoryIDs;
     @FXML
     public TableView<MedicalHistory> tableMedHistory;
+    @FXML
+    public Button buttonStartTest;
 
 
     @FXML
     public Pane paneCATTest;
     @FXML
-    public Slider coughScore;
+    public Slider sliderCoughScore;
     @FXML
-    public Slider mucusScore;
+    public Slider sliderMucusScore;
     @FXML
-    public Slider tightScore;
+    public Slider sliderTightScore;
     @FXML
-    public Slider breathlessScore;
+    public Slider sliderBreathlessScore;
     @FXML
-    public Slider activityLimitationScore;
+    public Slider sliderActivityLimitationScore;
     @FXML
-    public Slider confidentScore;
+    public Slider sliderConfidentScore;
     @FXML
-    public Slider sleepScore;
+    public Slider sliderSleepScore;
     @FXML
-    public Slider energyScore;
+    public Slider sliderEnergyScore;
+    @FXML
+    public Button buttonSendTest;
 
 
     public void setSelf(User u) throws Exception {
@@ -61,28 +64,27 @@ public class PatientController implements Initializable{
 
     @FXML
     public void chooseID() throws Exception {
+        hideAll();
         //show pane de la tabla con todos los medical history
         List<MedicalHistory> myMedHistory = new ArrayList<>(DbManager.getMedicalHistory(myself.getId()));
         //nose añadirlos a la tabla @Hugo para ti guapi y tb falta añadir los ids al combo box para que se pueda elegir :)
-        paneShowMedicalHist.setVisible(true);
-        paneCATTest.setVisible(false);
+        resetAndShowPaneMedicalHist();
     }
 
     @FXML
     public void newCATTest(){
-        medicalHistoryID = medicalHistoryIDs.getValue();
+        hideAll();
+        medicalHistoryID = comboBoxMedicalHistoryIDs.getValue();
         //show pane del CAT test como tal
-        paneShowMedicalHist.setVisible(false);
-        paneCATTest.setVisible(true);
+        resetAndShowPaneCatTest();
     }
 
     @FXML
     public void calculateCATTestScore() throws Exception {
-        int patient_severity = severityFromCAT(coughScore.getValue(), mucusScore.getValue(), tightScore.getValue(), breathlessScore.getValue(), activityLimitationScore.getValue(), confidentScore.getValue(), sleepScore.getValue(), energyScore.getValue());
+        int patient_severity = severityFromCAT(sliderCoughScore.getValue(), sliderMucusScore.getValue(), sliderTightScore.getValue(), sliderBreathlessScore.getValue(), sliderActivityLimitationScore.getValue(), sliderConfidentScore.getValue(), sliderSleepScore.getValue(), sliderEnergyScore.getValue());
         DbManager.updateSeverity(medicalHistoryID,patient_severity);
         //popup de sucess que nose lanzar y vuelta al inicio @Hugo
-        paneCATTest.setVisible(false);
-        paneShowMedicalHist.setVisible(false);
+        chooseID();
     }
 
     private int severityFromCAT(double cough, double mucus, double tight, double breathless, double activityLimitation, double confident, double sleep, double energy) {
@@ -96,6 +98,78 @@ public class PatientController implements Initializable{
         } else {
             return 4;
         }
+
+    }
+
+    private void hideAll(){
+        //panel medical history
+        paneShowMedicalHist.setVisible(false);
+        paneShowMedicalHist.setDisable(true);
+        tableMedHistory.setVisible(false);
+        tableMedHistory.setDisable(true);
+        comboBoxMedicalHistoryIDs.setVisible(false);
+        comboBoxMedicalHistoryIDs.setDisable(true);
+        buttonStartTest.setVisible(false);
+        buttonStartTest.setDisable(true);
+
+        //panel CAT TEST
+        paneCATTest.setVisible(false);
+        paneCATTest.setDisable(true);
+        sliderConfidentScore.setVisible(false);
+        sliderConfidentScore.setDisable(true);
+        sliderEnergyScore.setVisible(false);
+        sliderEnergyScore.setDisable(true);
+        sliderBreathlessScore.setVisible(false);
+        sliderBreathlessScore.setDisable(true);
+        sliderCoughScore.setVisible(false);
+        sliderCoughScore.setDisable(true);
+        sliderMucusScore.setVisible(false);
+        sliderMucusScore.setDisable(true);
+        sliderActivityLimitationScore.setVisible(false);
+        sliderActivityLimitationScore.setDisable(true);
+        sliderSleepScore.setVisible(false);
+        sliderSleepScore.setDisable(true);
+        sliderTightScore.setVisible(false);
+        sliderTightScore.setDisable(true);
+        buttonSendTest.setVisible(false);
+        buttonSendTest.setDisable(true);
+    }
+
+    private void resetAndShowPaneMedicalHist(){
+        paneShowMedicalHist.setVisible(true);
+        paneShowMedicalHist.setDisable(false);
+        tableMedHistory.setVisible(true);
+        tableMedHistory.setDisable(false);
+        comboBoxMedicalHistoryIDs.setVisible(true);
+        comboBoxMedicalHistoryIDs.setDisable(false);
+        buttonStartTest.setVisible(true);
+        buttonStartTest.setDisable(false);
+
+        comboBoxMedicalHistoryIDs.getSelectionModel().clearSelection();
+    }
+
+    private void resetAndShowPaneCatTest(){
+        paneCATTest.setVisible(true);
+        paneCATTest.setDisable(false);
+        sliderConfidentScore.setVisible(true);
+        sliderConfidentScore.setDisable(false);
+        sliderEnergyScore.setVisible(true);
+        sliderEnergyScore.setDisable(false);
+        sliderBreathlessScore.setVisible(true);
+        sliderBreathlessScore.setDisable(false);
+        sliderCoughScore.setVisible(true);
+        sliderCoughScore.setDisable(false);
+        sliderMucusScore.setVisible(true);
+        sliderMucusScore.setDisable(false);
+        sliderActivityLimitationScore.setVisible(true);
+        sliderActivityLimitationScore.setDisable(false);
+        sliderSleepScore.setVisible(true);
+        sliderSleepScore.setDisable(false);
+        sliderTightScore.setVisible(true);
+        sliderTightScore.setDisable(false);
+        buttonSendTest.setVisible(true);
+        buttonSendTest.setDisable(false);
+
     }
 
     
