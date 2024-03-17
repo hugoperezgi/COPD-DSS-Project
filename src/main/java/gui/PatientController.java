7635c6630a3608647c1823171eef327bc701fe58
hugop <h.perez10@usp.ceu.es>
2024-03-17 12:46:29 +0100

    Patient ui - java is empty :)

---
+++ src/main/java/gui/PatientController.java
@@ -0,0 +1,24 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import entities.*;
+import javafx.fxml.Initializable;
+
+public class PatientController implements Initializable{
+
+    public Patient myself;
+
+    public void setSelf(User u) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSelf'");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+    
+}
